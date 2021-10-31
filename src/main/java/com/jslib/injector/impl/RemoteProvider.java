0e41132e2f5b942d24b62332ad021b3aee2cff43
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2021-10-31 13:12:14 +0200

    Throw illegal state if there is no remote factory for protocol.

--- src/main/java/com/jslib/injector/impl/RemoteProvider.java
+++ src/main/java/com/jslib/injector/impl/RemoteProvider.java
@@ -1,6 +1,5 @@
 package com.jslib.injector.impl;
 
-import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ServiceLoader;
@@ -13,41 +12,49 @@ import js.log.LogFactory;
 import js.rmi.RemoteFactory;
 import js.rmi.RemoteFactoryProvider;
 import js.rmi.UnsupportedProtocolException;
+import js.util.Params;
 import js.util.Strings;
 
-class RemoteProvider<T> implements Provider<T> {
-	private static final Log log = LogFactory.getLog(RemoteProvider.class);
+class RemoteProvider<T> implements Provider<T>
+{
+  private static final Log log = LogFactory.getLog(RemoteProvider.class);
 
-	private static final Map<String, RemoteFactory> remoteFactories = new HashMap<>();
-	static {
-		for (RemoteFactoryProvider provider : ServiceLoader.load(RemoteFactoryProvider.class)) {
-			for (String protocol : provider.getProtocols()) {
-				if (remoteFactories.put(protocol, provider.getRemoteFactory()) != null) {
-					throw new BugError("Invalid runtime environment. Remote factory protocol override |%s|. See remote factory provider |%s|.", protocol, provider);
-				}
-			}
-		}
-	}
+  private static final Map<String, RemoteFactory> remoteFactories = new HashMap<>();
+  static {
+    for(RemoteFactoryProvider provider : ServiceLoader.load(RemoteFactoryProvider.class)) {
+      for(String protocol : provider.getProtocols()) {
+        if(remoteFactories.put(protocol, provider.getRemoteFactory()) != null) {
+          throw new BugError("Invalid runtime environment. Remote factory protocol override |%s|. See remote factory provider |%s|.", protocol, provider);
+        }
+      }
+    }
+  }
 
-	private final RemoteFactory remoteFactory;
-	private final Class<T> type;
-	private final String implementationURL;
+  private final RemoteFactory remoteFactory;
+  private final Class<T> type;
+  private final String implementationURL;
 
-	public RemoteProvider(Class<T> type, String implementationURL) {
-		log.trace("RemoteProvider(Class<T>, String)");
-		
-		String protocol = Strings.getProtocol(implementationURL);
-		if (protocol == null) {
-			throw new UnsupportedProtocolException(new MalformedURLException("Protocol not found on " + implementationURL));
-		}
-		this.remoteFactory = remoteFactories.get(protocol);
+  public RemoteProvider(Class<T> type, String implementationURL)
+  {
+    log.trace("RemoteProvider(Class<T>, String)");
+    Params.notNullOrEmpty(implementationURL, "Implementation URL");
 
-		this.type = type;
-		this.implementationURL = implementationURL;
-	}
+    String protocol = Strings.getProtocol(implementationURL);
+    if(protocol == null) {
+      throw new UnsupportedProtocolException("Protocol not found on " + implementationURL);
+    }
+    this.remoteFactory = remoteFactories.get(protocol);
+    if(this.remoteFactory == null) {
+      throw new IllegalStateException("No remote factory for protocol " + protocol);
+    }
 
-	@Override
-	public T get() {
-		return remoteFactory.getRemoteInstance(type, implementationURL);
-	}
+    this.type = type;
+    this.implementationURL = implementationURL;
+  }
+
+  @Override
+  public T get()
+  {
+    return remoteFactory.getRemoteInstance(type, implementationURL);
+  }
 }
