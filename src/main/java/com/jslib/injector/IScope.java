7b800a91405bbbe99d8de6eb279f407ea1fcd1d0
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2021-12-30 14:44:02 +0200

    Add support for both normal and pseudo-scopes.

--- src/main/java/com/jslib/injector/IScope.java
+++ src/main/java/com/jslib/injector/IScope.java
@@ -2,6 +2,12 @@ package com.jslib.injector;
 
 import java.lang.annotation.Annotation;
 
+/**
+ * Meta interface for <code>scope</code> annotations, both {@link jakarta.enterprise.context.NormalScope normal scope}
+ * and {@link jakarta.inject.Scope pseudo-scopes}. This utility interface supports both Jakarta and Javax packages.
+ * 
+ * @author Iulian Rotaru
+ */
 public interface IScope
 {
 
@@ -12,7 +18,19 @@ public interface IScope
 
   static boolean isPresent(Class<? extends Annotation> annotation)
   {
-    return annotation.isAnnotationPresent(jakarta.inject.Scope.class) || annotation.isAnnotationPresent(javax.inject.Scope.class);
+    if(annotation.isAnnotationPresent(jakarta.enterprise.context.NormalScope.class)) {
+      return true;
+    }
+    if(annotation.isAnnotationPresent(javax.enterprise.context.NormalScope.class)) {
+      return true;
+    }
+    if(annotation.isAnnotationPresent(jakarta.inject.Scope.class)) {
+      return true;
+    }
+    if(annotation.isAnnotationPresent(javax.inject.Scope.class)) {
+      return true;
+    }
+    return false;
   }
 
 }
